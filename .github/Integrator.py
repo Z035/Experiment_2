bec709e4d6bf4cde2c5e04cabb6874a36bcaa9b6
Z035 <phoebusxp@gmail.com>
2023-03-29 10:52:08 -0400

    Class Material

---
+++ .github/Integrator.py
@@ -0,0 +1,23 @@
+import abc
+
+
+class Integrator(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, model, dt):
+        """
+
+        :param model: model obj with a rhs() method that takes t,y and returns dy/dt
+        :param dt:
+        """
+        self.model = model
+        self.dt = dt
+
+    @abc.abstractmethod
+    def step(self, t, y):
+        """
+        Take a step
+        :param t: current time
+        :param y: vector of state
+        :return: y at time t+dt
+        """
+        pass
